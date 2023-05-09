4c42eda47a159410635c16407717936565020ef8
famousDeer <dawidtrzcinski1@gmail.com>
2023-05-09 16:56:33 +0200

    First commit

--- Program/SSD/detect.py
+++ Program/SSD/detect.py
@@ -1,7 +1,6 @@
 from torchvision import transforms
 from util import *
 from PIL import Image, ImageDraw, ImageFont
-import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -97,7 +96,7 @@ def detect(original_image, min_score, max_overlap, top_k, suppress=None):
 
 
 if __name__ == '__main__':
-    img_path = '/media/ssd/ssd data/VOC2007/JPEGImages/000001.jpg'
+    img_path = '/home/famousdeer/Desktop/Praca magisterska/Program/data/VOCdevkit/VOC2007/JPEGImages/000005.jpg'
     original_image = Image.open(img_path, mode='r')
     original_image = original_image.convert('RGB')
     detect(original_image, min_score=0.2, max_overlap=0.5, top_k=200).show()
