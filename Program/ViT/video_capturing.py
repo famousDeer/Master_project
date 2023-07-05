59e720154321b934fa7019f9c499e5da7790a51f
famousDeer <dawidtrzcinski1@gmail.com>
2023-07-05 20:16:44 +0200

    few changes

--- Program/ViT/video_capturing.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-img = cv2.VideoCapture("http://192.168.1.106:4747/video")
-
-while True:
-    ret, frame = img.read()
-    cv2.imshow("frame", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
