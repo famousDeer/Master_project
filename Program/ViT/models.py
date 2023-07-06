6cf79ed35d0afaef9417031592bce15e8e656af2
famousDeer <dawidtrzcinski1@gmail.com>
2023-07-06 22:41:19 +0200

    Added PatchEmbedding, MSA, MLP

--- Program/ViT/models.py
+++ Program/ViT/models.py
@@ -6,6 +6,14 @@ from torchsummary import summary
 
 
 class PatchEmbedding(nn.Module):
+    """Turns a 2D input image into a 1D sequence learnable embedding vector.
+    Using simple Convolution and Flatten from nn.
+    
+    Args:
+        in_channels (int): Number of color channels for the input images. Defaults to 3.
+        patch_size (int): Size of patches to convert input image into. Defaults to 16.
+        embedding_dim (int): Size of embedding to turn image into. Defaults to 768.
+    """
     def __init__(self,
                  in_channels: int=3, 
                  patch_size: int=16, 
@@ -34,3 +42,85 @@ class PatchEmbedding(nn.Module):
         x_flattened = self.flatten(x_patched)
 
         return x_flattened.permute(0,2,1)
+
+class MultiheadSelfAttentionBlock(nn.Module):
+    def __init__(self,
+                 embedding_dim: int=768,
+                 num_heads: int=12,
+                 dropout: float=0.0) -> None:
+        super(MultiheadSelfAttentionBlock, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.num_heads = num_heads
+        self.dropout = dropout
+        
+        self.layer_norm = nn.LayerNorm(normalized_shape=self.embedding_dim)
+        self.multihead_attn = nn.MultiheadAttention(embed_dim=self.embedding_dim,
+                                                    num_heads=self.num_heads,
+                                                    dropout=self.dropout,
+                                                    batch_first=True)
+    
+    def forward(self, x):
+        x = self.layer_norm(x)
+        attn_output, _ = self.multihead_attn(query=x,
+                                             key=x,
+                                             value=x,
+                                             need_weights=False)
+        
+        return attn_output
+
+class MLPBlock(nn.Module):
+    def __init__(self,
+                 embedding_dim: int=768,
+                 mlp_size: int=3072,
+                 dropout: float=0.1) -> None:
+        super(MLPBlock, self).__init__()
+
+        self.embedding_dim = embedding_dim
+        self.mlp_size = mlp_size
+        self.dropout = dropout
+
+        self.layer_norm = nn.LayerNorm(normalized_shape=self.embedding_dim)
+        self.mlp = nn.Sequential(
+            nn.Linear(in_features=self.embedding_dim,
+                      out_features=self.mlp_size),
+            nn.GELU(),
+            nn.Dropout(p=self.dropout),
+            nn.Linear(in_features=self.mlp_size,
+                      out_features=self.embedding_dim),
+            nn.Dropout(p=self.dropout)
+        )
+
+    def forward(self, x):
+        x = self.layer_norm(x)
+        x = self.mlp(x)
+        
+        return x
+    
+class ViT(nn.Module):
+    def __init__(self,
+                 img_height: int,
+                 img_width: int,
+                 batch_size: int,
+                 patch_size: int=16,
+                 embedding_dim: int=768) -> None:
+        super(ViT,self).__init__()
+        
+        self.batch_size = batch_size
+        self.img_height = img_height
+        self.img_width = img_width
+        self.patch_size = patch_size
+        self.number_patches = (self.img_height * self.img_width) / self.patch_size ** 2
+        self.embedding_dim = embedding_dim
+        
+        self.class_embedding = nn.Parameter(data=torch.randn(self.batch_size, 
+                                                             1, 
+                                                             self.embedding_dim),
+                                            requires_grad=True)
+        self.position_embedding = nn.Parameter(data=torch.randn(1,
+                                                                self.number_patches+1,
+                                                                self.embedding_dim),
+                                               requires_grad=True)
+        
+    def forward(self, x):
+        
+        return x
--- Program/ViT/transformer.py
+++ Program/ViT/transformer.py
@@ -1,11 +1,15 @@
 import torch
 import torchvision
 import matplotlib.pyplot as plt
+import os
+import random
 
 from torch import nn
 from torchvision import transforms
 from torchinfo import summary
 from helper_functions import plot_images, set_seed
+from pathlib import Path
+from models import PatchEmbedding
 
 # Choose device 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -15,7 +19,15 @@ print(f'DEVICE: {device}')
 set_seed(seed=100)
 
 # Custom parameters
-SHOW_ORIGINAL_IMAGES = True
+SHOW_ORIGINAL_IMAGES = False
+HEIGHT = 320
+WIDTH = 320
+COLOR_CHANNELS = 3
+PATCH_SIZE = 16
+NUMBER_OF_PATCHES = int((HEIGHT * WIDTH) / PATCH_SIZE**2)
+PATCHES_PER_ROW = HEIGHT // PATCH_SIZE
+EMBEDDING_DIM = PATCH_SIZE * PATCH_SIZE * COLOR_CHANNELS
+print(f"Number of patches (N) with image height (H={HEIGHT}), width (W={WIDTH}) and patch size (P={PATCH_SIZE}): {NUMBER_OF_PATCHES}")
 
 # Hyper-parameters
 NUM_EPOCHS = 10
@@ -24,35 +36,106 @@ LEARNING_RATE = 0.001
 
 # Image preprocessing modules
 transform = transforms.Compose([
+    transforms.Resize((HEIGHT,WIDTH)),
     transforms.Pad(4),
     transforms.RandomHorizontalFlip(),
-    transforms.RandomCrop(32),
+    transforms.RandomCrop(320),
     transforms.ToTensor()])
 print(f"Used transform: {transform}")
-
+ROOT = Path('/home/famousdeer/Desktop/Praca magisterska/Program/data/Cityscape')
 # CIFAR-10 dataset
-train_dataset = torchvision.datasets.CIFAR10(root='../data/',
-                                             train=True, 
-                                             transform=transform,
-                                             download=True)
+# train_dataset = torchvision.datasets.CIFAR10(root='../data/',
+#                                              train=True, 
+#                                              transform=transform,
+#                                              download=True)
 
-test_dataset = torchvision.datasets.CIFAR10(root='../data/',
-                                            train=False, 
-                                            transform=transforms.ToTensor())
+# test_dataset = torchvision.datasets.CIFAR10(root='../data/',
+#                                             train=False, 
+#                                             transform=transforms.ToTensor())
+
+train_dataset = torchvision.datasets.Cityscapes(root=ROOT,
+                                                split='train',
+                                                mode='fine',
+                                                target_type='instance',
+                                                transform=transform)
+
+test_dataset = torchvision.datasets.Cityscapes(root=ROOT,
+                                               split='test',
+                                               mode='fine',
+                                               target_type='instance',
+                                               transform=transform)
 
 # Data loader
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=BATCH_SIZE,
-                                           shuffle=True)
+                                           shuffle=True,
+                                           num_workers=os.cpu_count())
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=BATCH_SIZE,
-                                          shuffle=False)
-print(f"{100*'='}\nTRAIN DATA: {len(train_dataset.data)}")
-print(f"TEST DATA: {len(test_dataset.data)}")
+                                          shuffle=False,
+                                          num_workers=os.cpu_count())
+
+print(f"{100*'='}\nTRAIN DATA: {len(train_dataset)}")
+print(f"TEST DATA: {len(test_dataset)}")
 print(f"CLASSES: {test_dataset.classes}\n{100*'='}")
+
+###SHOW_SOME_RESULTS###
 
 # Show few images with labels
 if SHOW_ORIGINAL_IMAGES:
     plot_images(train_dataset, 3)
+# Original Image
+plt.imshow(train_dataset[0][0].permute(1,2,0))
+
+conv2d = nn.Conv2d(in_channels=3,
+                   out_channels=768,
+                   kernel_size=PATCH_SIZE,
+                   stride=PATCH_SIZE,
+                   padding=0)
+random_indexes = random.sample(range(0, 768), k=25)
+print(f"Showing random convolutional feature maps from indexes: {random_indexes}")
+
+fig, axs = plt.subplots(nrows=5, ncols=5, figsize=(12,12))
+
+# Turn image into feature maps
+image_out_of_conv = conv2d(train_dataset[0][0].unsqueeze(0))
+print(f"Image feature map shape: {image_out_of_conv.shape}")
+
+# Flatten the feature maps on height and width
+flatten = nn.Flatten(start_dim=2,
+                     end_dim=3)
+image_out_of_flatten = flatten(image_out_of_conv)
+image_out_of_flatten_reshaped = image_out_of_flatten.permute(0,2,1)
+print(f"Flattend image feature map shape: {image_out_of_flatten_reshaped.shape}")
+
+single_flattened_feature_map = image_out_of_flatten_reshaped[:,:,0]
+plt.figure(figsize=(20,20))
+plt.imshow(single_flattened_feature_map.detach().numpy())
+plt.axis('off')
+for row in range(5):
+    for col in range(5):
+        image_conv_feature_map=image_out_of_conv[:,random_indexes[row+col],:,:]
+        axs[row,col].imshow(image_conv_feature_map.squeeze().detach().numpy())
+        axs[row,col].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+        axs[row,col].set_title(random_indexes[5*row+col])
+plt.show()
+
+####START_MODEL####
+# Create an instance of patch embedding layer
+patch_embed = PatchEmbedding(in_channels=COLOR_CHANNELS,
+                             patch_size=PATCH_SIZE,
+                             embedding_dim=EMBEDDING_DIM)
+
+# Check if everything is correct 
+print(f"Input image shape: {train_dataset[0][0].unsqueeze(0).shape}")
+patch_embedded_image = patch_embed(train_dataset[0][0].unsqueeze(0))
+print(f"Output patch embedding shape: {patch_embedded_image.shape}")
+
+# Model summary for single image 
+summary(model=PatchEmbedding(),
+        input_size=train_dataset[0][0].unsqueeze(0).shape,
+        col_names=['input_size', 'output_size', 'num_params', 'trainable'],
+        col_width=20,
+        row_settings=['var_names'])
 
